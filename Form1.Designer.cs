5721ff9b3148718d09e5060e24fc8e0d1960fa83
NekokeCore <Core2002@163.com>
2022-04-25 14:32:59 +0800

    修改标题的我的世界为Minecraft

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -148,7 +148,7 @@ namespace PlayerDataBackupTool_CSharp
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "我的世界服务器玩家背包备份还原工具";
+            this.Text = "Minecraft服务器玩家背包备份还原工具";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
