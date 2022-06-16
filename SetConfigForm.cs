e25545d316cc2d84227c9a9e2aaaff33b12d582f
NekokeCore <Core2002@163.com>
2022-06-16 15:00:14 +0800

    把网络请求改成异步

--- SetConfigForm.cs
+++ SetConfigForm.cs
@@ -2,8 +2,7 @@
 using Newtonsoft.Json.Linq;
 using System;
 using System.IO;
-using System.Net;
-using System.Text;
+using System.Net.Http;
 using System.Windows.Forms;
 
 namespace PlayerDataBackupTool_CSharp
@@ -41,30 +40,32 @@ namespace PlayerDataBackupTool_CSharp
             uuid2name_path.Text = Form1.cfg.uuid2name_path;
         }
 
+        int pathsNumber = 0;
         private void button2_Click(object sender, EventArgs e)
         {
+            button2.Enabled = false;
             stats = (0, 0);
+            pathsNumber = 0;
             string[] paths = Directory.GetFiles(Form1.cfg.world_playerdata_path);
             foreach (var item in paths)
             {
                 if (Path.GetExtension(item).ToLower() == ".dat")
                 {
+                    pathsNumber++;
                     runUnit(Path.GetFileNameWithoutExtension(item));
                 }
             }
-            label6.Text = $"{stats.Item1}Sec,{stats.Item2}Err,OK!";
         }
 
         //成功，失败
         (int, int) stats;
-        private void runUnit(string uuid)
+        private async void runUnit(string uuid)
         {
-            string jsonchuan = QueryPostparamsService("https://api.mojang.com/user/profile/" + uuid);
-            //JObject jo = (JObject)JsonConvert.DeserializeObject(jsonchuan); //跟下方一行效果一样，两种写法，区别是什么我也不知道
-            //把json转为这个JObject类型，并放在 jo 里
-
             try
             {
+                HttpClient client = new HttpClient();
+                client.Timeout = TimeSpan.FromSeconds(10);
+                string jsonchuan = await client.GetStringAsync("https://api.mojang.com/user/profile/" + uuid);
                 JObject jo = JObject.Parse(jsonchuan);
                 Form1.Singleton.dic.Add(uuid, jo["name"].ToString());
                 File.WriteAllText(Form1.cfg.uuid2name_path, JsonConvert.SerializeObject(Form1.Singleton.dic,Formatting.Indented));
@@ -74,36 +75,17 @@ namespace PlayerDataBackupTool_CSharp
             {
                 stats.Item2++;
             }
-            label6.Text = $"{stats.Item1}Sec,{stats.Item2}Err,Runing......";
+            if(pathsNumber == stats.Item1 + stats.Item2)
+            {
+                label6.Text = $"{stats.Item1}Sec,{stats.Item2}Err,OK";
+                button2.Enabled = true;
+            }
+            else
+            {
+                label6.Text = $"{stats.Item1}Sec,{stats.Item2}Err,Runing......{pathsNumber}";
+            }
+            
         }
 
-        //定义一个请求api并获取返回内容的方法，url作为参数方便测试不同url
-        public static string QueryPostparamsService(string url)
-        {
-            //定义一个result用来存放接收到的json数据
-            string result = "";
-            //请求url
-            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
-            //请求方法为Get
-            req.Method = "Get";
-            //这里没有添加请求头，注释掉了
-            //req.Headers.Add()
-            //使用try-catch捕获异常情况
-            try
-            {
-                HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
-                Stream stream = resp.GetResponseStream();
-                //获取内容
-                using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
-                {
-                    result = reader.ReadToEnd();
-                }
-            }
-            catch (Exception ex)
-            {
-                result = ex.Message;
-            }
-            return result;
-        }
     }
 }
